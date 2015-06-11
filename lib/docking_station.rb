72848ad4f8d12f771cd0910a23a065e21bd209f9
Simon Legg <leggsimon@gmail.com>
2015-06-11 13:00:18 +0100

    Update DockingStation to have capacity of 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,18 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock bike
-    @bike = bike
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
