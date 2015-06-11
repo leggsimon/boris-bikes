7f044c64a082cb1db5b98c7b83e823ffe9f2aa51
Simon Legg <leggsimon@gmail.com>
2015-06-11 13:30:54 +0100

    Added DockingStation::DEFAULT_CAPACITY and attr_reader

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,18 +14,23 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument}
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+
   describe 'release_bike' do
     it 'raises an error when there are no bikes available' do
       expect {subject.release_bike}.to raise_error 'No bikes available'
     end
   end
 
-    describe 'dock' do
-      it 'raises an error when full' do
-        20.times {subject.dock Bike.new}
-        expect {subject.dock Bike.new}.to raise_error 'Docking station full'
-      end
+  describe 'dock' do
+    it 'raises an error when full' do
+      subject.capacity.times {subject.dock Bike.new}
+      expect {subject.dock Bike.new}.to raise_error 'Docking station full'
     end
+  end
 
 
 end
